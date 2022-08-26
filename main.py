cd54503ab10246dd0b637c600f37a4edb3430651
smmehrab <mehrab.24csedu.001@gmail.com>
2022-08-26 20:33:38 +0600

    Tabular Statistics Implemented

--- main.py
+++ main.py
@@ -170,28 +170,26 @@ def descriptive_statistics():
     print()
     print()
 
-# def tabular_statistics():
-#     print('{:<35}'.format("Tabular Statistics"))
-#     print('{:<35}'.format("------------------------"))
-#     print()
-#     print("As we've generated the insert-search sequence randomly\nif we take the performance measures of the first 100 \ninsert-search sequence for both of the algorithms, we\nshould get a useful comparison between both algorithms\nbased on some random insert-search sequence.\n\nSo, the comparison is given below:")
-#     print()
+def tabular_statistics():
+    print('{:<35}'.format("Tabular Statistics"))
+    print('{:<35}'.format("------------------------"))
+    print()
+    print("As we've generated the insert-search sequence randomly\nif we take the performance measures of the last 100 \ninsert-search sequence for both of the algorithms, we\nshould get a useful comparison between both algorithms\nbased on some random insert-search sequence.\n\nBecause taking last 100 insert-search will ensure that\nwe are comparing between a largely populated double\nhash table & a largely populated red black tree over a\nrandom set of operations on some random set of data.\n\nSo, the comparison is given below:")
+    print()
 
-#     print('{:<50}'.format("------------------------------------------------------------"))
-#     print(' {:<15}    {:<20}    {:<20}'.format("Insert-Search", "Double Hashing", "Red Black Tree"))
-#     print('{:<50}'.format("------------------------------------------------------------"))
+    print('{:<50}'.format("------------------------------------------------------------"))
+    print(' {:<15}    {:<20}    {:<20}'.format("Insert-Search", "Double Hashing", "Red Black Tree"))
+    print('{:<50}'.format("------------------------------------------------------------"))
 
-#     index = 0
-#     while(index<NUMBER_OF_ROWS_IN_DATA_COMPARISON_TABLE):
-#         print(' {:<15}    {:<20}    {:<20}'.format(index+1, DOUBLE_HASH_PERFORMANCE_DATA[index], RED_BLACK_TREE_PERFORMANCE_DATA[index]))
-#         index += 1
+    offset = len(DOUBLE_HASH_PERFORMANCE_DATA)-100
+    index = 0
+    while(index<NUMBER_OF_ROWS_IN_DATA_COMPARISON_TABLE):
+        print(' {:<15}    {:<20}    {:<20}'.format(offset+index+1, DOUBLE_HASH_PERFORMANCE_DATA[offset+index], RED_BLACK_TREE_PERFORMANCE_DATA[offset+index]))
+        index += 1
 
-#     print('{:<50}'.format("------------------------------------------------------------"))
-#     print()
-#     print()
-
-
-
+    print('{:<50}'.format("------------------------------------------------------------"))
+    print()
+    print()
 
 def visual_statistics():
     pass
@@ -214,7 +212,7 @@ def display_statistics():
     print()
 
     descriptive_statistics()
-    # tabular_statistics()
+    tabular_statistics()
     # visual_statistics()
 
 
